d0691835b37ae593fadf0fa7371fffa10e725196
m-guseva <maja.guseva@bccn-berlin.de>
2023-05-04 15:22:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# DudleWebScraper
+ This script scrapes the entries in a Dudle and sorts them.
---
+++ dudleScrape.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from requests_html import HTMLSession
+
+
+def main():
+    names = []
+    Ticks = []
+    Positions = []
+    #create the session
+    session = HTMLSession()
+
+    #define our URL
+    url = 'INSERT_YOUR_DUDLE_URL_HERE'
+
+    #use the session to get the data
+    r = session.get(url)
+
+    #get number of participants to feed into loop
+    nParticipant = len(r.html.find('.participantrow'))
+
+
+    for j in range(nParticipant):
+        #Get names and meeting durations:
+        participantRow = r.html.find('.participantrow')
+        trInfo = participantRow[j].find('td')
+        names.append(trInfo[1].text) #list of names with durations
+
+        #get tick marks:
+        ticks = []
+        P = participantRow[j].find('td') 
+        for i in range(2,17):
+            a = (P[i].find('.vote'))
+            ticks.append(a[0].text) #list of tick marks
+        Ticks.append(ticks)
+        
+
+    for i in range(len(Ticks)):
+        Positions.append(Ticks[i].index('✔')) #get position of tick marks
+
+    #compile all to dataframe:
+    Results = pd.DataFrame({'positions': Positions, 'names':names}).sort_values(by=['positions'])
+    print(Results)
+
+
+if __name__ == "__main__":
+     main() 
